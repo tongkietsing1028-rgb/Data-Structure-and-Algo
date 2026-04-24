a86ea0a67d2079a0776922a300036e9af77ed247
TongKietSing <tongkietsing1028@gmail.com>
2026-04-24 14:59:28 +0800

    test sort

--- RadixSort/RadixSort.cpp
+++ RadixSort/RadixSort.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include<stdlib.h>
 using namespace std;
 
 int getMax(int arr[], int n)
@@ -38,11 +39,26 @@ void CountSort(int arr[], int exp, int n)
 	}
 }
 
-void RadixFunction(int arr[], int exp, int n)
+void RadixFunction(int arr[],  int n)
 {
 	int m = getMax(arr,n);
-	for (exp = 1;exp / m > 0;exp *= 10)
+	for (int exp = 1;m / exp > 0;exp *= 10)
 	{
 		CountSort(arr, exp, n);
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	int arr[20];
+	for (int i = 0;i < 20;i++)
+	{
+		arr[i] = rand() % 100;
+	}
+	RadixFunction(arr, 20);
+	for (int i = 0; i < 20; i++)
+	{
+		cout << arr[i] << " ";
 	}
 }
